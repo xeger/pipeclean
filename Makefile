c15ef21bb8466d72b04289dd1e214201cdc79cf9
Tony Spataro <github.com@tracker.xeger.net>
2023-03-21 00:05:33 -0700

    Prototype scrubbing + restore

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 default:
+	cat data.sql | go run main.go
+
+diff:
+	cat data.sql | go run main.go > data2.sql
+	diff data.sql data2.sql | head -n 1
+
+test:
 	cat data.sql | go run main.go > data2.sql
 	diff data.sql data2.sql
--- main.go
+++ main.go
@@ -2,13 +2,64 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
+	"github.com/pingcap/tidb/parser/format"
+	"github.com/pingcap/tidb/parser/test_driver"
 	_ "github.com/pingcap/tidb/parser/test_driver"
 )
+
+// Preserves non-parseable lines (assuming they are comments).
+const COMMENTS = true
+
+// Preserves INSERT statements (disable to make debug printfs readable).
+const INSERTS = true
+
+// Preserves non-insert lines (LOCK/UNLOCK/SET/...).
+const MISC = true
+
+// Does silly trivial scrambling as POC
+const REVERSE = false
+
+type scrubber struct {
+}
+
+func (v *scrubber) Enter(in ast.Node) (ast.Node, bool) {
+	switch st := in.(type) {
+	case *test_driver.ValueExpr:
+		switch st.Kind() {
+		case test_driver.KindString:
+			scrubbed := test_driver.Datum{}
+			scrubbed.SetString(v.scrubString(st.Datum.GetString()))
+			return &test_driver.ValueExpr{Datum: scrubbed}, true
+		}
+	}
+	return in, false
+}
+
+func (v *scrubber) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func (v *scrubber) scrubString(s string) string {
+	// TODO: recognize (& sanitize?) all well-formed YAML, JSON
+	if strings.Index(s, "\n") >= 0 {
+		return s
+	}
+	if REVERSE {
+		result := ""
+		for _, v := range s {
+			result = string(v) + result
+		}
+		return result
+	}
+	return s
+}
 
 func parse(sql string) (ast.StmtNode, error) {
 	p := parser.New()
@@ -26,12 +77,29 @@ func parse(sql string) (ast.StmtNode, error) {
 }
 
 func sanitize(stmt ast.StmtNode) ast.StmtNode {
-	switch stmt.(type) {
-	case *ast.UpdateStmt:
-		// TODO: sanitize update statement
-		return stmt
+	switch st := stmt.(type) {
+	case *ast.InsertStmt:
+		v := &scrubber{}
+		// fmt.Printf("%+v\n", st.Table.TableRefs.Left.(*ast.TableSource).Source.(*ast.TableName).Name)
+		// fmt.Printf("%d %+v\n", len(st.Lists), st.Lists[0][0])
+		st.Accept(v)
+		buf := new(bytes.Buffer)
+		ctx := format.NewRestoreCtx(format.DefaultRestoreFlags, buf)
+		st.Restore(ctx)
+		s := buf.String()
+		s = strings.ReplaceAll(s, "\n", "\\n")
+		s = s + ";"
+		if INSERTS {
+			st, _ := parse(s)
+			return st
+		} else {
+			return nil
+		}
 	default:
-		return stmt
+		if MISC {
+			return stmt
+		}
+		return nil
 	}
 }
 
@@ -45,9 +113,14 @@ func main() {
 
 		stmt, _ := parse(line)
 		if stmt != nil {
-			fmt.Println(stmt.Text())
+			stmt = sanitize(stmt)
+			if stmt != nil {
+				fmt.Println(stmt.Text())
+			}
 		} else {
-			fmt.Print(line)
+			if COMMENTS {
+				fmt.Print(line)
+			}
 		}
 	}
 }
