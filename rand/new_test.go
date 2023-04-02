cfb41f6745b35f3915b07104a078a70e952f78e7
Tony Spataro <github.com@tracker.xeger.net>
2023-04-02 16:56:23 -0700

    Prove determinism of local rand package

--- rand/new_test.go
+++ rand/new_test.go
@@ -7,18 +7,19 @@ import (
 )
 
 func TestNewSource(t *testing.T) {
+	N := 8
 	output := make([][]int64, 256)
 	for i := 0; i < 256; i++ {
-		output[i] = make([]int64, 256)
-		for j := 0; j < 256; j++ {
+		output[i] = make([]int64, N)
+		for j := 0; j < N; j++ {
 			output[i][j] = rand.NewSource("").Int63()
 		}
 	}
-	tgt := output[0]
+	tgt := []int64{2761073929007780571, 2761073929007780571, 2761073929007780571, 2761073929007780571, 2761073929007780571, 2761073929007780571, 2761073929007780571, 2761073929007780571}
 	for i := 1; i < len(output); i++ {
-		for j := 0; j < 256; j++ {
+		for j := 0; j < N; j++ {
 			if tgt[j] != output[i][j] {
-				t.Fatalf("rand.NewSource().Int63() is not deterministic")
+				t.Fatalf("rand.NewSource().Int63() is not deterministic %v", output[i])
 				break
 			}
 		}
