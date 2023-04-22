71cf06a924bce1c28454bae4f82caff6de6ed930
Tony Spataro <github.com@tracker.xeger.net>
2023-04-22 07:48:58 -0700

    Deal with enormously long lines

--- format/mysql/extract.go
+++ format/mysql/extract.go
@@ -23,9 +23,13 @@ func Extract(ctx *Context, names []string, r io.Reader, w io.Writer) {
 	p := parser.New()
 	v := &extractVisitor{ctx, names, nil, nil}
 
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		values := extract(v, p, scanner.Text())
+	br := bufio.NewReader(r)
+	for {
+		line, err := br.ReadString('\n')
+		if err != nil {
+			break
+		}
+		values := extract(v, p, line)
 		for _, v := range values {
 			w.Write([]byte(v + "\n"))
 		}
